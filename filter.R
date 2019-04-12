0e924c53e889329af037fc67eb6302d07883d59b
Tony Lam <tonyjhlam@gmail.com>
2019-04-12 19:13:53 -0400

    add gender filter

--- filter.R
+++ filter.R
@@ -2,6 +2,7 @@
 
 # load data 
 df <- read.csv('dataset_diabetes/diabetic_data.csv')
+map <- read.csv('dataset_diabetes/IDs_mapping.csv')
 df[df == '?'] = NA
 df[df == 'None'] = NA
 
@@ -11,4 +12,11 @@ df <- df[,colSums(is.na(df)) == 0]
 # relabel readmitted
 df$readmitted = ifelse(df$readmitted=='NO',0,1)
 
+# remove columns
+df <- df[ , !(names(df) %in% c('discharge_disposition_id', 'admission_source_id'))]
+
+#delete invalid gender
+df <- df[- grep("Unknown/Invalid", df$gender),]
+
+# rename 
 write.csv(df, file='dataset_diabetes/diabetic_data.filtered.csv')
